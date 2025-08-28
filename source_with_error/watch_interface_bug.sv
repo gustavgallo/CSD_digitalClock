8452247edc9f0aea0d067eeba604f041a5a2357a
gustavgallo <gustavotibollagallo@gmail.com>
2025-08-28 18:48:35 -0300

    go

--- source_with_error/watch_interface_bug.sv
+++ source_with_error/watch_interface_bug.sv
@@ -1,6 +1,6 @@
 // Versão do relógio com bugs
 
-module watch_interface ( //talvez seja melhor mudar o nome para watch, para não confundir com clock
+module watch_interface_bug ( //talvez seja melhor mudar o nome para watch, para não confundir com clock
 
 input logic clock, // 100 MHz clock
 input logic reset, // reset 
